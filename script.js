faa3b294bb69de9aa26902682df6322b02a5f13d
Isaiah Cruz <isaiahglenncruz@gmail.com>
2024-05-01 23:31:34 -0700

    working with smoothscroll offsets to fix navbar interactions

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Get all project cards
+////////////////////////////// EXPAND CURRENT PROJECT CARD
 const projects = document.querySelectorAll('.project');
-
-// Function to check if an element is in the middle of the screen
+// check if element is in the middle of the screen
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
@@ -11,8 +10,7 @@ function isElementInViewport(el) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
-
-// Function to expand project card
+// expanding the project card if it is in middle of screen:3
 function expandProject() {
     projects.forEach(project => {
         if (isElementInViewport(project)) {
@@ -22,9 +20,41 @@ function expandProject() {
         }
     });
 }
-
-// Call expandProject function on scroll
 window.addEventListener('scroll', expandProject);
 
-// CONTACT ME FORM
-///////////////////////
+////////////////////////////////// MOVING HEADER WHILE SCROLLING 
+var header = document.querySelector('.header');
+var headerPosition = header.offsetTop;
+
+function handleScroll() { // function to check whether header is in place or not
+    if (window.pageYOffset > headerPosition) {
+        header.classList.add('fixed-header'); // if it is, fix the header at the top of the page
+    } else {
+        header.classList.remove('fixed-header'); // if not, return the header to its original position
+    }
+}
+window.addEventListener('scroll', handleScroll);
+
+
+////////////////////////////// SMOOTH SCROLL TO FUNCTIONS
+function smoothScroll(target) {
+    const headerHeight = document.querySelector('.header').offsetHeight;
+    const targetElement = document.querySelector(target);
+    const offsetTop = targetElement.offsetTop - headerHeight; // header height - target's offset
+
+    const additionalOffset = 70; // ADJUST
+
+    window.scroll({
+        top: offsetTop - additionalOffset, // Subtract additional offset
+        behavior: 'smooth'
+    });
+}
+const navLinks = document.querySelectorAll('.nav-button'); // all header's navigation links
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const target = this.getAttribute('href');
+        smoothScroll(target);
+    });
+});
